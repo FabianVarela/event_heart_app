21c7aaf979ae5b199cbe3ee0de8992942f21b59c
fabianvarela <jfabian.303@gmail.com>
2020-05-25 16:52:35 -0500

    Create city model

---
+++ lib/model/city.dart
@@ -0,0 +1,13 @@
+class City {
+  City({this.id, this.name});
+
+  final int id;
+  final String name;
+}
+
+List<City> cities = <City>[
+  City(id: 1, name: 'Bogotá'),
+  City(id: 2, name: 'Medellín'),
+  City(id: 3, name: 'Cali'),
+  City(id: 4, name: 'Cartagena'),
+];
