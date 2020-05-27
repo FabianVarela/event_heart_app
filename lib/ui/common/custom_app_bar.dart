2d75e8c07b9c918537ae12e8ebd7459583fa9e2a
fabianvarela <jfabian.303@gmail.com>
2020-05-26 23:22:32 -0500

    Create custom app bar

---
+++ lib/ui/common/custom_app_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                'EH',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Atone',
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.share,
+                size: 30,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
