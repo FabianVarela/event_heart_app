be876ef72463ef520a2627ba383886da6059b731
fabianvarela <jfabian.303@gmail.com>
2020-05-27 23:03:34 -0500

    Adding new property, isSelected

--- lib/model/event.dart
+++ lib/model/event.dart
@@ -9,6 +9,7 @@ class Event {
     this.color,
     this.type,
     this.description,
+    this.isSelected,
   ]);
 
   final String name;
@@ -18,6 +19,7 @@ class Event {
   final Color color;
   final String type;
   final String description;
+  final bool isSelected;
 }
 
 class EventDate {
@@ -45,6 +47,7 @@ List<Event> events = <Event>[
     Color(0xFF350938),
     'Concierto',
     'Panic! at the disco es un grupo estadounidense de rock formado en Las Vegas en 2004 por Ryan Ross y Spencer Smith. Tras varios cambios en su formación, desde 2015 está integrado únicamente por el cantante Brendon Urie, a quien en las giras acompañan Nicole Row en bajo, Mike Naran en guitarra y Dan Pawlovich en batería.',
+    false,
   ),
   Event(
     'Estereo Picnic',
@@ -67,6 +70,7 @@ List<Event> events = <Event>[
     Color(0xFF380914),
     'Concierto',
     'Creado en el año 2010 por un grupo independiente de promotores de eventos, con muchas dificultades sobre todo en las primeras ediciones, el "Festival Estéreo Picnic" se ha venido realizando en la Sabana de Bogotá con artistas nacionales e internacionales que ofrecen música Rock, Electrónica, Pop y Nuevo Folcklore entre muchos otros.',
+    false,
   ),
   Event(
     'Guns n\' Roses',
@@ -81,6 +85,7 @@ List<Event> events = <Event>[
     Color(0xFF160938),
     'Concierto',
     'Guns N\' Roses es una banda estadounidense de hard rock formada en Hollywood en la zona de Sunset Strip alrededor de Santa Monica, en la ciudad de Los Ángeles, California en 1985. El grupo fue fundado por el vocalista Axl Rose y el guitarrista Izzy Stradlin.',
+    true,
   ),
   Event(
     'Flutter Hackathon',
@@ -95,6 +100,7 @@ List<Event> events = <Event>[
     Color(0xFF093819),
     'Tecnología',
     'Join us for this years Online International Flutter Hackathon!',
+    true,
   ),
   Event(
     'Flutter Conf',
@@ -113,6 +119,7 @@ List<Event> events = <Event>[
     Color(0xFF093138),
     'Tecnología',
     'FlutterConf es la primera conferencia sobre Flutter que congregará a todos los entusiastas, desarrolladores y empresas que están apostando por esta increible tecnología.',
+    true,
   ),
   Event(
     'Tomorrowland',
@@ -131,6 +138,7 @@ List<Event> events = <Event>[
     Color(0xFF203809),
     'Concierto',
     'Tomorrowland es un festival de música electrónica de baile celebrado anualmente en la localidad de Boom (Bélgica). El festival es organizado por ID&T y se calcula que anualmente acuden más de 400.000 personas de casi 200 nacionalidades distintas.',
+    false,
   ),
   Event(
     'UMF Miami',
@@ -149,6 +157,7 @@ List<Event> events = <Event>[
     Color(0xFF381D09),
     'Concierto',
     'Ultra Music Festival (UMF) es un festival anual de música electrónica, fundado en 1999 por la pareja de negocios Russell Faibisch y Alex Omes. El festival tiene lugar en marzo en la ciudad de Miami, Florida.',
+    true,
   ),
   Event(
     'Burning man',
@@ -171,6 +180,7 @@ List<Event> events = <Event>[
     Color(0xFF383109),
     'Concierto',
     'El festival Burning Man (en español: "Hombre en llamas") es un evento anual de siete días de duración que se desarrolla en la "ciudad" de Black Rock, Nevada, Estados Unidos. Black Rock City (o "BRC"), que no es un municipio ni tiene gobierno, solo existe durante la semana de Burning Man, es decir, es una ciudad temporal construida por los participantes hasta el primer lunes de septiembre.',
+    false,
   ),
 ];
 
