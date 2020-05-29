b87541a681f48efb0c57ad9a005615517b112dba
fabianvarela <jfabian.303@gmail.com>
2020-05-28 23:35:41 -0500

    Modify main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:event_heart_app/ui/home.ui.dart';
-import 'package:event_heart_app/ui/my_events.ui.dart';
-import 'package:event_heart_app/ui/select_city.ui.dart';
+import 'package:event_heart_app/ui/screen_home/home.ui.dart';
+import 'package:event_heart_app/ui/screen_my_event/my_events.ui.dart';
+import 'package:event_heart_app/ui/screen_select_city/select_city.ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
       title: 'Event Heart',
       theme: ThemeData(
         textTheme: GoogleFonts.montserratTextTheme(Theme.of(context).textTheme),
-        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       initialRoute: '/',
