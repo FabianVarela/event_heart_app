ea59697d580ffb7a663540b30858a8cf6f85e5db
fabianvarela <jfabian.303@gmail.com>
2020-05-28 23:33:48 -0500

    Create palette color class

---
+++ lib/utils/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF707070);
+  static const Color red = Color(0xFFE21F35);
+  static const Color black = Color(0xFF000000);
+}
