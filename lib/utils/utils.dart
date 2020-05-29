244c8ce4d3914ad782ea2ebbf4558f33a053c683
fabianvarela <jfabian.303@gmail.com>
2020-05-28 23:56:22 -0500

    Refactor util date methods to use intl package

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,64 +1,15 @@
+import 'package:intl/intl.dart';
+
 class Utils {
-  static String getMonthString(DateTime dateTime) {
-    switch (dateTime.month) {
-      case 1:
-        return 'ENE';
-      case 2:
-        return 'FEB';
-      case 3:
-        return 'MAR';
-      case 4:
-        return 'ABR';
-      case 5:
-        return 'MAY';
-      case 6:
-        return 'JUN';
-      case 7:
-        return 'JUL';
-      case 8:
-        return 'AGO';
-      case 9:
-        return 'SEP';
-      case 10:
-        return 'OCT';
-      case 11:
-        return 'NOV';
-      case 12:
-        return 'DIC';
-      default:
-        return '';
-    }
-  }
+  static String getMonthString(DateTime dateTime) =>
+      DateFormat.MMMM('es').format(dateTime).substring(0, 3).toUpperCase();
 
-  static int getDayMonth(DateTime dateTime) {
-    return dateTime.day;
-  }
+  static String getDayMonth(DateTime dateTime) =>
+      DateFormat.d('es').format(dateTime);
 
-  static String getDayString(DateTime dateTime) {
-    switch (dateTime.weekday) {
-      case 1:
-        return 'Lunes';
-      case 2:
-        return 'Martes';
-      case 3:
-        return 'Miércoles';
-      case 4:
-        return 'Jueves';
-      case 5:
-        return 'Viernes';
-      case 6:
-        return 'Sábado';
-      case 7:
-        return 'Domingo';
-      default:
-        return '';
-    }
-  }
+  static String getDayString(DateTime dateTime) =>
+      DateFormat.EEEE('es').format(dateTime);
 
-  static String getHourString(DateTime dateTime) {
-    final String minute = dateTime.minute.toString().length <= 1
-        ? '0${dateTime.minute}'
-        : '${dateTime.minute}';
-    return '${dateTime.hour}:$minute';
-  }
+  static String getHourString(DateTime dateTime) =>
+      DateFormat.Hm().format(dateTime);
 }
